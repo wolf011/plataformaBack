36eb0ca61322184fcbbf9a6ed8b3e629072cd170
Hugo Braga <hugobraga11@hotmail.com>
2025-10-16 16:26:12 -0300

    uploading the project's structure

---
+++ loja/src/main/java/br/com/loja/loja/entity/Produto.java
@@ -0,0 +1,14 @@
+package br.com.loja.loja.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long Id;
+}
